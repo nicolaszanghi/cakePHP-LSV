930a15695992d14fffe84a3c5e7a041ef2746c4e
nicolaszanghi <nicolas.zanghi@gmail.com>
2013-11-20 08:00:11 +0100

    cakestrap helper locale

--- app/View/Helper/CakestrapHelper.php
+++ app/View/Helper/CakestrapHelper.php
@@ -8,22 +8,29 @@ class CakestrapHelper extends FormHelper {
         parent::setEntity($fieldName);
         $options = parent::_parseOptions($options);
 
-        if (!isset($options['placeholder']))
-            $options['placeholder'] = strip_tags(parent::_getLabel($fieldName, $options));
-
+        /** class */
         if (empty($options['class'])) $options['class'] = '';
         if (empty($options['div']['class'])) $options['div']['class'] = '';
 
+        /** placeholder */
+        if (!isset($options['placeholder']))
+            $options['placeholder'] = strip_tags(parent::_getLabel($fieldName, $options));
+
+
         $options['div']['class'] .= ' input '.$options['type'];
 
+        /** radio, checkbox */
         if (in_array($options['type'], array('radio', 'checkbox'))) {
             $label = strip_tags(parent::_getLabel($fieldName, $options));
             $options['label'] = false;
             $options['before'] = '<label>';
             $options['after'] = $label.'</label>';
+
+            /** multiple checkbox */
         } elseif (!empty($options['multiple']) && $options['multiple'] == 'checkbox') {
             $options['class'] .= ' checkbox';
 
+            /** date/time => datetimeplicker */
         } elseif (in_array($options['type'], array('datetime', 'date', 'time'))) {
 
             $before_label = parent::_getLabel($fieldName, $options);
@@ -37,6 +44,7 @@ class CakestrapHelper extends FormHelper {
                                  </span>';
             $options['type'] = 'text';
 
+            /** search */
         } elseif ($options['type'] == 'search') {
 
             $options['label'] = false;
@@ -47,6 +55,7 @@ class CakestrapHelper extends FormHelper {
                                  </span>';
             $options['type'] = 'text';
 
+            /** normal input */
         } else {
             $options['div']['class'] .= ' form-group';
             $options['class'] .= ' form-control';
@@ -54,11 +63,39 @@ class CakestrapHelper extends FormHelper {
 
         $options['required'] = false;
 
+        /** help-block */
         if (!empty($options['help-block']))
             $options['after'] .= "\n\t".'<p class="help-block">'.$options['help-block'].'</p>';
 
-        $output = parent::input($fieldName, $options);
 
+        /** if local option is true display an input per languages */
+        if (!empty($options['locale'])) {
+            $output = '';
+            $placeholder = $options['placeholder'];
+            $label = (empty($options['label'])) ? strip_tags(parent::_getLabel($fieldName, $options)) : $options['label'];
+
+            // get translations, for edit
+            if (!empty($this->request->data)) {
+                $translations_tmp = $this->request->data[end(explode('.', $fieldName)).'Translation'];
+                $translations = array();
+                foreach ($translations_tmp as $translation)
+                    $translations[$translation['locale']] = $translation['content'];
+            }
+
+            foreach (Configure::read('Config.languages') as $k => $v) { // eng => English, fra => Français
+                if (!empty($placeholder)) $options['placeholder'] = $placeholder.' ('.$v.')';
+                if (!empty($label)) $options['label'] = $label.' ('.$v.')';
+                if (!empty($translations)) $options['value'] = $translations[$k];
+                $output .= parent::input($fieldName.'.'.$k, $options);
+            }
+
+
+            /** normal output (without translation) */
+        } else {
+            $output = parent::input($fieldName, $options);
+        }
+
+        /** before label */
         if (!empty($before_label)) // label outside div
             $output = $before_label.$output;
 
